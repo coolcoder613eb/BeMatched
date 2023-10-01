e587a7ea2ce6fe1de4acda7c6e44c93326ae0166
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2023-10-01 22:23:45 +1100

    Rearrange setup window

--- main.py
+++ main.py
@@ -59,14 +59,18 @@ class SetupWindow(BWindow):
         self.panel = BView(self.Bounds(), "panel", 8, 20000000)
         #self.label_1 = BStringView(BRect(50, 10, 150, 30), "label 1", "Name Of Player 1")
         self.input_1 = BTextControl(
-            BRect(50, 10, 250, 20), "input 1", "Name Of Player 1", "player1", BMessage(OK_MSG)
+            BRect(10, 10, 330, 20), "input 1", "Name Of Player 1", "player1", BMessage(OK_MSG)
+        )
+        self.input_2 = BTextControl(
+            BRect(10, 60, 330, 20), "input 2", "Name Of Player 2", "player2", BMessage(OK_MSG)
         )
         #be_plain_font.SetSize(be_plain_font.Size()-5)
         #self.input_1.SetFont(be_plain_font)
         self.ok_button = BButton(
-            BRect(40, 90, 100, 50), "ok_button", "Ok", BMessage(OK_MSG)
+            BRect(120, 100, 210, 50), "ok_button", "Ok", BMessage(OK_MSG)
         )
         self.panel.AddChild(self.input_1, None)
+        self.panel.AddChild(self.input_2, None)
         self.panel.AddChild(self.ok_button, None)
         self.AddChild(self.panel, None)
         self.Show()
@@ -83,6 +87,8 @@ class SetupWindow(BWindow):
         print("Started!")
         should_close=False
         self.Quit()
+
+
 
 class MatchIt(BApplication):
     def __init__(self):
